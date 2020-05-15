e3496aa1dec1092229d6cc3d4e66897322912699
Krittanan Pingclasai <kp@kpping.me>
2020-05-15 07:56:29 +0000

    update format

--- utils.ts
+++ utils.ts
@@ -4,6 +4,7 @@ import { pipe } from 'fp-ts/lib/pipeable'
 import { last } from 'fp-ts/lib/Array';
 import { getOrElse, map } from 'fp-ts/lib/Option';
 import { IO } from 'fp-ts/lib/IO';
+import { isNil } from '@ag1/nil';
 
 // get incorrect propery path
 export function getPropPath(context: t.Context): string {
@@ -15,9 +16,18 @@ export function getExpected(context: t.Context): string {
     return pipe(Array.from(context), last, map((c) => c.type.name), getOrElse(() => 'any'));
 }
 
+// if val is not nil, put it in bracket
+export function formatActualValue(val: unknown): string {
+    if (isNil(val)) {
+        return `${val}`;
+    }
+
+    return `${typeof val}(${val})`;
+}
+
 // convert Error object to String
 export function toString(errors: t.Errors): string {
-    return errors.map((e) => `Invalid prop: ${getPropPath(e.context)}\t>> Expected: ${getExpected(e.context)} != Actual: ${String(e.value)}`).join('\n') + '\n';
+    return errors.map((e) => `Invalid prop: ${getPropPath(e.context)}\t>> Expected: ${getExpected(e.context)} <> Actual: ${formatActualValue(e.value)}`).join('\n') + '\n';
 };
 
 // log to console
